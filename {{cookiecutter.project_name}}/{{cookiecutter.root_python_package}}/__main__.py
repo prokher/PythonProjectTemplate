3e164d1239761fbfa910de1bb5401c382004acd5
Alexander A. Prokhorov <prokher@gmail.com>
2020-03-30 00:03:43 +0300

    Initial package entrypoint added - `__main__.py`.

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.root_python_package}}/__main__.py
@@ -0,0 +1,9 @@
+"""Entrypoint of the {{cookiecutter.root_python_package}} package."""
+
+
+def main():
+    """Package {{cookiecutter.root_python_package}} entrypoint."""
+
+
+if __name__ == "__main__":
+    main()
