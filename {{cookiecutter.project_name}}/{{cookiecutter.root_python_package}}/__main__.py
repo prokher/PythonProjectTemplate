ae21d9eb9f5b6eb219bef108e734e11e8f77f2ef
Alexander A. Prokhorov <prokher@gmail.com>
2020-03-30 01:18:22 +0300

    Package entrypoint added.

--- {{cookiecutter.project_name}}/{{cookiecutter.root_python_package}}/__main__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.root_python_package}}/__main__.py
@@ -1,8 +1,13 @@
-"""Entrypoint of the {{cookiecutter.root_python_package}} package."""
+"""Entrypoint of the `{{cookiecutter.root_python_package}}` package."""
 
 
 def main():
-    """Package {{cookiecutter.root_python_package}} entrypoint."""
+    """Package `{{cookiecutter.root_python_package}}` entrypoint.
+
+    This main entrypoint function executes when you run
+    `python -m {{cookiecutter.root_python_package}}`.
+
+    """
 
 
 if __name__ == "__main__":
